a949f75cb14df50fc35a7723ed037f3f4e0c8fe2
Daniel Brüggemann <github@barfooz.com>
2017-07-09 21:44:26 +0200

    [App] Add very basic and rough version of rucksack

---
+++ src/js/background.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const PAGES = {
+    dashboard: '/html/dashboard.html'
+};
+
+function onClicked() {
+    collectAllNotPinnedTabs();
+    openPage('dashboard');
+}
+
+function collectAllNotPinnedTabs() {
+    browser
+        .tabs
+        .query({ currentWindow: true })
+        .then(items => {
+            const noPinnedTabs = items.filter(noPinnedTab);
+            saveTabsToLocalStorage(noPinnedTabs);
+
+            browser.tabs.remove(noPinnedTabs.map(tab => tab.id));
+        });
+}
+
+function openPage(pageName) {
+    browser.tabs.create({
+        url: PAGES[pageName]
+    });
+}
+
+
+function noPinnedTab(item) {
+    return !item.pinned;
+}
+
+function saveTabsToLocalStorage(tabsToSave) {
+    browser.storage.local.get('tabs').then(storage => {
+        let tabs = [ tabsToSave ];
+
+        if (storage.hasOwnProperty('tabs')) {
+            tabs = [ ...storage.tabs, ...tabs ]
+        }
+
+        browser.storage.local.set({ tabs });
+    });
+}
+
+browser.browserAction.onClicked.addListener(onClicked);
---
+++ src/js/dashboard.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const $TabListContainer = document.querySelector('#TabListContainer');
+const $ClearStorage = document.querySelector('#ClearStorage');
+const $ReloadStorage = document.querySelector('#ReloadStorage');
+
+let TABS = [];
+
+function renderTabLists() {
+    browser.storage.local.get('tabs').then(storage=> {
+        if (!storage.hasOwnProperty('tabs')) {
+            $TabListContainer.innerHTML = '';
+            return false;
+        }
+
+        let lists = [];
+
+        TABS = storage.tabs;
+
+        TABS.map((list, index) => {
+            lists = [ ...lists, renderTabList(list, index) ];
+        });
+
+        $TabListContainer.innerHTML = lists.join('');
+        $TabListContainer.addEventListener('click', openTabs);
+    });
+}
+
+function renderTabList(list, id) {
+    let items = [];
+
+    list.map(item => {
+        items = [ ...items, renderTabListItem(item.url, item.title) ];
+    });
+
+    return `
+        <div>
+            <ul>${items.join('')}</ul>
+            <button class="open-tabs" data-id="${id}">
+                Open list #${id}
+            </button>
+        </div>
+    `;
+}
+
+function openTabs(event) {
+    if (event.target.classList.contains('open-tabs')) {
+        const id = +event.target.dataset.id;
+        const tabsToOpen = TABS[id];
+
+        tabsToOpen.map(tab => {
+            browser.tabs.create({
+                active: false,
+                url: tab.url
+            });
+        });
+
+        TABS = [
+            ...TABS.slice(0, id),
+            ...TABS.slice(id + 1)
+        ];
+
+        browser.storage.local.set({ tabs: TABS }).then(() => renderTabLists());
+    }
+}
+
+function renderTabListItem(href, content) {
+    return `
+        <li>
+            <a href="${href}">${content}</a>
+        </li>
+    `;
+}
+
+function onClearStorageClick() {
+    browser.storage.local.clear().then(() => renderTabLists());
+}
+
+function onReloadStorageClick() {
+    renderTabLists();
+}
+
+$ClearStorage.addEventListener('click', onClearStorageClick);
+$ReloadStorage.addEventListener('click', onReloadStorageClick);
+
+renderTabLists();
