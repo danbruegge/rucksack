2bff2cb4c85f854b7868b8efe2a06d09c35a761e
Daniel Brüggemann <github@barfooz.com>
2017-07-10 20:37:00 +0200

    [App] Add block scope for `background.js` and `dashboard.js`

--- src/js/background.js
+++ src/js/background.js
@@ -1,45 +1,47 @@
-const PAGES = {
-    dashboard: '/html/dashboard.html'
-};
+{
+    const PAGES = {
+        dashboard: '/html/dashboard.html'
+    };
 
-function onClicked() {
-    collectAllNotPinnedTabs();
-    openPage('dashboard');
-}
+    function onClicked() {
+        collectAllNotPinnedTabs();
+        openPage('dashboard');
+    }
 
-function collectAllNotPinnedTabs() {
-    browser
-        .tabs
-        .query({ currentWindow: true })
-        .then(items => {
-            const noPinnedTabs = items.filter(noPinnedTab);
-            saveTabsToLocalStorage(noPinnedTabs);
+    function collectAllNotPinnedTabs() {
+        browser
+            .tabs
+            .query({ currentWindow: true })
+            .then(items => {
+                const noPinnedTabs = items.filter(noPinnedTab);
+                saveTabsToLocalStorage(noPinnedTabs);
 
-            browser.tabs.remove(noPinnedTabs.map(tab => tab.id));
+                browser.tabs.remove(noPinnedTabs.map(tab => tab.id));
+            });
+    }
+
+    function openPage(pageName) {
+        browser.tabs.create({
+            url: PAGES[pageName]
         });
+    }
+
+
+    function noPinnedTab(item) {
+        return !item.pinned;
+    }
+
+    function saveTabsToLocalStorage(tabsToSave) {
+        browser.storage.local.get('tabs').then(storage => {
+            let tabs = [ tabsToSave ];
+
+            if (storage.hasOwnProperty('tabs')) {
+                tabs = [ ...storage.tabs, ...tabs ]
+            }
+
+            browser.storage.local.set({ tabs });
+        });
+    }
+
+    browser.browserAction.onClicked.addListener(onClicked);
 }
-
-function openPage(pageName) {
-    browser.tabs.create({
-        url: PAGES[pageName]
-    });
-}
-
-
-function noPinnedTab(item) {
-    return !item.pinned;
-}
-
-function saveTabsToLocalStorage(tabsToSave) {
-    browser.storage.local.get('tabs').then(storage => {
-        let tabs = [ tabsToSave ];
-
-        if (storage.hasOwnProperty('tabs')) {
-            tabs = [ ...storage.tabs, ...tabs ]
-        }
-
-        browser.storage.local.set({ tabs });
-    });
-}
-
-browser.browserAction.onClicked.addListener(onClicked);
